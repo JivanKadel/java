fd7b576bc0350d477ff210527adfc00ca1596f31
JivanKadel <jivankadel256@gmail.com>
2024-05-25 18:23:14 +0545

    Update UserInput.java
    "renamed to UserInput "

--- java basics/UserInput.java
+++ java basics/UserInput.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class UsrInput {
+public class UserInput {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter an integer");

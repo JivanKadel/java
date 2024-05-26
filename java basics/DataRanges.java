dc93df7deec611a72f6543ecd5ca3a85e7d9b5a6
JivanKadel <jivankadel256@gmail.com>
2024-05-26 10:49:15 +0545

    Added a new DataRanges Class

---
+++ java basics/DataRanges.java
@@ -0,0 +1,13 @@
+public class DataRanges {
+    public static void main(String[] args) {
+        System.out.println("Byte minimum: " + Byte.MIN_VALUE);
+        System.out.println("Byte maximum: " + Byte.MAX_VALUE);
+        System.out.println("Byte bytes: " + Byte.BYTES);
+        System.out.println("Int maximum: " + Integer.MIN_VALUE);
+        System.out.println("Int minimum: " + Integer.MAX_VALUE);
+        System.out.println("Int bytes: " + Integer.BYTES);
+        System.out.println("Float maximum: " + Float.MIN_VALUE);
+        System.out.println("Float minimum: " + Float.MAX_VALUE);
+        System.out.println("Float bytes: " + Float.BYTES);
+    }
+}

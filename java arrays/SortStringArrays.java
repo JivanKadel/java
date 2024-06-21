d8551ce3eff5ec601de0ef511b756deedcc67a65
JivanKadel <jivankadel256@gmail.com>
2024-06-21 14:28:18 +0545

    String sorting in java

---
+++ java arrays/SortStringArrays.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortStringArrays {
+    public static void main(String[] args) {
+        String[] arr = {"java", "swift", "basic", "python", "pascal", "javascript", "c"};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}

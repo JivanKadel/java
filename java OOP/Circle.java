cbcfdfb709e2893cf1251cec9e2a3450f6cf4852
JivanKadel <jivankadel256@gmail.com>
2024-06-24 13:56:51 +0545

    Inheritance using Circle, Cylinder

---
+++ java OOP/Circle.java
@@ -0,0 +1,22 @@
+public class Circle {
+    public double radius;
+    public double area(){
+        return Math.PI * radius * radius;
+    }
+    public double perimeter(){
+        return  2 * Math.PI * radius;
+    }
+
+    public static void main(String[] args) {
+        Cylinder c = new Cylinder();
+        c.radius = 7;
+        c.height = 10;
+        System.out.println("Volume = " + c.volume());
+    }
+}
+class Cylinder extends  Circle{
+    public double height;
+    public double volume(){
+        return area() * height;
+    }
+}
